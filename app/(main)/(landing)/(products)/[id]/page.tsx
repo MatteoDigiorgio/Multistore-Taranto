a1d56ff8a7e2f20997de0c7e404c84da2bb1289e
MatteoDigiorgio <matthew.digiorgio@gmail.com>
2023-05-26 20:40:17 +0200

    Set the foundation for product page

--- app/(main)/(landing)/(products)/[id]/page.tsx
+++ app/(main)/(landing)/(products)/[id]/page.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+"use client";
+import MyLoading from "@/app/(main)/MyLoading";
+import { getProduct } from "@/pages/api/auth/getProducts";
+import { Prodotto } from "@/types";
+import React, { useEffect, useState } from "react";
 
-function Product() {
-  return <div className="flex justify-center text-2xl">Prodotto</div>;
+function Product({ params }: any) {
+  const [product, setProduct] = useState<Prodotto>();
+  const [immagineUrl, setImmagineUrl] = useState();
+
+  const getRandomNumber = () => {
+    const min = 1;
+    const max = 4;
+    // Generate a random decimal between 0 (inclusive) and 1 (exclusive)
+    const randomDecimal = Math.random();
+    // Scale the random decimal to be between 0 (inclusive) and (max - min) exclusive
+    const scaledDecimal = randomDecimal * (max - min);
+    // Round down the scaled decimal to the nearest integer
+    const randomNumber = Math.floor(scaledDecimal) + min;
+    return randomNumber;
+  };
+
+  let random = getRandomNumber();
+  const [isLoading, setIsLoading] = useState(random);
+
+  useEffect(() => {
+    async function fetchData(params: any) {
+      const prodottiData = await getProduct(params.id);
+
+      setTimeout(() => {
+        prodottiData ? setImmagineUrl(prodottiData.immagineUrl) : null;
+        delete prodottiData.immagineUrl;
+        prodottiData ? setProduct(prodottiData) : null;
+        setIsLoading(0);
+      }, 1000);
+    }
+    fetchData(params);
+  }, []);
+
+  console.log(product);
+  return (
+    <>
+      <div className="bg-white flex justify-center items-center flex-grow z-0">
+        {product ? (
+          product.nome
+        ) : (
+          <>
+            <MyLoading isLoading={isLoading} />
+          </>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Product;
--- app/(main)/(landing)/Landing.tsx
+++ app/(main)/(landing)/Landing.tsx
@@ -3,7 +3,7 @@ import React, { Suspense, useEffect, useState } from "react";
 import Products from "./(products)/Products";
 import { Prodotto } from "@/types";
 import { getProducts } from "@/pages/api/auth/getProducts";
-import styles from "./Loading.module.css";
+import MyLoading from "../MyLoading";
 
 function Landing() {
   const [products, setProducts] = useState<Prodotto[]>();
@@ -42,64 +42,7 @@ function Landing() {
         </div>
       ) : (
         <>
-          {/* Tech */}
-          <div className={`${isLoading === 1 ? "flex" : "hidden"}`}>
-            <div className={styles.loader_tech}></div>
-            <div className={styles.loader_tech}></div>
-            <div className={styles.loader_tech}></div>
-          </div>
-          {/* Washing Machine */}
-          <div
-            className={`${
-              isLoading === 2
-                ? "flex items-center justify-center drop-shadow-sm"
-                : "hidden"
-            }`}
-          >
-            <div className={styles.loader_washing}></div>
-          </div>
-          {/* Logo */}
-          <div
-            className={`${
-              isLoading === 3 ? "flex items-center justify-center" : "hidden"
-            }`}
-          >
-            <div className={styles.loader_multiLogo}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                version="1.0"
-                viewBox="0 0 500 500"
-                width="150"
-                height="150"
-                fill="currentColor"
-                className={styles.logo}
-              >
-                <g>
-                  <path d="M 134.091 489.074 L 133.809 82.1 L 38.797 150.334 L 38.909 420.787 L 134.091 489.074 Z"></path>
-                  <line
-                    x1="39.062"
-                    y1="420.695"
-                    x2="133.966"
-                    y2="351.978"
-                  ></line>
-                </g>
-                <g>
-                  <path d="M 246.245 369.941 L 178.824 320.954 C 178.824 320.954 178.771 53.369 178.783 53.369 C 178.795 53.369 245.155 5.287 245.155 5.287 L 313.53 56.464 L 313.609 320.732 L 246.245 369.941 Z"></path>
-                  <line x1="245.712" y1="272.955" x2="245.154" y2="5.31"></line>
-                  <polyline points="178.86 320.919 245.708 272.64 313.621 320.735"></polyline>
-                </g>
-                <g>
-                  <line
-                    x1="356.169"
-                    y1="353.019"
-                    x2="450.792"
-                    y2="420.781"
-                  ></line>
-                  <path d="M 356.245 488.865 L 355.984 82.16 L 451.065 150.244 L 451.06 420.977 L 356.245 488.865 Z"></path>
-                </g>
-              </svg>
-            </div>
-          </div>
+          <MyLoading isLoading={isLoading} />
         </>
       )}
     </>
