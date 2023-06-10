977d079e069f57835c25d6816a7649bebdecf151
MatteoDigiorgio <matthew.digiorgio@gmail.com>
2023-06-10 15:35:41 +0200

    Search 'engine' implemented. Now search result more relatable

--- pages/api/auth/getProducts.ts
+++ pages/api/auth/getProducts.ts
@@ -1,5 +1,6 @@
 import { getDownloadURL, ref } from "firebase/storage";
 import db, { storage } from "../../../firebase";
+import { compareTwoStrings } from "string-similarity"; // Library for token-based matching
 
 export async function getProducts() {
   const dbProdotti = await db
@@ -87,16 +88,81 @@ export async function getFilterProducts(filter: any) {
           myObject[key] = value;
         }
       }
-
+      console.log(myObject);
       myObject.id = doc.id;
+      myObject.score = calculateSearchScore(lowercaseFilter, myObject);
       return myObject;
     })
   );
+  // Define the threshold score
+  const thresholdScore = 20; // Adjust this value as needed
 
-  // Filter the products based on the partial match
-  const filteredProdotti = prodotti.filter((prodotto) =>
-    prodotto.nome.toLowerCase().includes(lowercaseFilter)
+  // Filter the products based on the threshold score
+  const filteredProducts = prodotti.filter(
+    (product) => product.score >= thresholdScore
   );
 
-  return filteredProdotti;
+  // Sort the filtered products based on the score in descending order
+  const sortedProducts = filteredProducts.sort((a, b) => b.score - a.score);
+
+  return sortedProducts;
+}
+
+function calculateSearchScore(filter: string, object: any): number {
+  let score = 0;
+  let matchedFields = 0;
+  const objectValues = Object.values(object);
+
+  for (const value of objectValues) {
+    if (
+      typeof value === "string" &&
+      value !== "id" &&
+      value !== "immagine" &&
+      value !== "prezzo" &&
+      value !== "sconto" &&
+      value !== "percentuale"
+    ) {
+      const distance = levenshteinDistance(filter, value.toLowerCase());
+      score += 1 / (distance + 1); // Assign higher score for closer matches
+
+      const lowercasedValue = value.toLowerCase();
+      if (lowercasedValue.includes(filter)) {
+        score += 50; // Increase score if filter is found in the value
+      }
+
+      matchedFields++; // Increment the count of matched fields
+    }
+  }
+
+  // Add the number of matched fields to the score
+  score += matchedFields;
+
+  return score;
+}
+
+function levenshteinDistance(a: string, b: string): number {
+  const m = a.length;
+  const n = b.length;
+  const dp = [];
+
+  for (let i = 0; i <= m; i++) {
+    dp.push([i]);
+  }
+
+  for (let j = 1; j <= n; j++) {
+    dp[0][j] = j;
+  }
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      const cost = a[i - 1] === b[j - 1] ? 0 : 1;
+      dp[i][j] = Math.min(
+        dp[i - 1][j] + 1, // deletion
+        dp[i][j - 1] + 1, // insertion
+        dp[i - 1][j - 1] + cost // substitution
+      );
+    }
+  }
+
+  return dp[m][n];
 }
