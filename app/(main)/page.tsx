49c708c6e9ab90d23f1ca29db561b3b5af25775e
MatteoDigiorgio <matthew.digiorgio@gmail.com>
2023-06-27 20:40:40 +0200

    Product page code simplified + Footer fixed for product page and homepage

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ReactElement } from "react";
 import MainLanding from "./(landing)/MainLanding";
 import getGoogleData from "./getGoogleData";
+import Footer from "./prodotti/Footer";
 
 export default async function Home(): Promise<ReactElement> {
   const data: object = await getGoogleData();
@@ -10,6 +11,7 @@ export default async function Home(): Promise<ReactElement> {
       <div className="bg-white flex justify-center flex-grow z-0">
         <main className="w-full">
           <MainLanding data={data} />
+          <Footer />
         </main>
       </div>
     </>
