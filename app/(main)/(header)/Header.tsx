7af0a6392b5ef153ef31412eb5116607cb5c859a
MatteoDigiorgio <matthew.digiorgio@gmail.com>
2023-08-28 22:01:34 +0200

    Minor fix

--- app/(main)/(header)/Header.tsx
+++ app/(main)/(header)/Header.tsx
@@ -1,26 +1,26 @@
-"use client";
-import React, { useState, useRef } from "react";
-import Link from "next/link";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import Menu from "./Menu";
-import Image from "next/image";
-import { useDispatch } from "react-redux";
-import { usePathname, useRouter } from "next/navigation";
-import styles from "./Header.module.css";
-import { HomeIcon } from "@heroicons/react/24/outline";
-import { update } from "../../../slices/searchSlice";
-import { update as googleUpdate } from "@/slices/googleSlice";
+'use client';
+import React, { useState, useRef } from 'react';
+import Link from 'next/link';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import Menu from './Menu';
+import Image from 'next/image';
+import { useDispatch } from 'react-redux';
+import { usePathname, useRouter } from 'next/navigation';
+import styles from './Header.module.css';
+import { HomeIcon } from '@heroicons/react/24/outline';
+import { update } from '../../../slices/searchSlice';
+import { update as googleUpdate } from '@/slices/googleSlice';
 
 export const MultistoreLogo = () => {
   return (
-    <div className="flex justify-center items-center mx-4 mt-4 md:w-1/6 md:mt-0 sm:flex-grow-0 md:justify-start">
-      <Link title="Home" passHref href="/">
+    <div className='flex justify-center items-center mx-4 mt-4 md:w-1/6 md:mt-0 sm:flex-grow-0 md:justify-start'>
+      <Link title='Home' passHref href='/'>
         <Image
-          alt="Multistore Taranto Logo"
-          src="/multistore_logo.png"
+          alt='Multistore Taranto Logo'
+          src='/multistore_logo.png'
           width={120}
           height={50}
-          className="object-contain cursor-pointer"
+          className='object-contain cursor-pointer'
           unoptimized={true}
         />
       </Link>
@@ -29,7 +29,7 @@ export const MultistoreLogo = () => {
 };
 
 export const SearchBar = () => {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
   const router = useRouter();
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -37,10 +37,10 @@ export const SearchBar = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const trimmedValue = input.replace(/\s+/g, " ").trim();
+    const trimmedValue = input.replace(/\s+/g, ' ').trim();
     dispatch(update(trimmedValue));
-    setInput("");
-    router.push("/prodotti");
+    setInput('');
+    router.push('/prodotti');
     if (inputRef.current) {
       inputRef.current.blur(); // Hide the keyboard
     }
@@ -57,21 +57,21 @@ export const SearchBar = () => {
     >
       <input
         ref={inputRef}
-        className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-full text-base focus:outline-none px-4 placeholder:italic placeholder:text-slate-400"
-        type="text"
-        name="search"
-        placeholder="Cerca Prodotto, Modello, Brand"
+        className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-full text-base focus:outline-none px-4 placeholder:italic placeholder:text-slate-400 border-0 focus:ring-0'
+        type='text'
+        name='search'
+        placeholder='Cerca Prodotto, Modello, Brand'
         value={input}
         onChange={handleChange}
       />
       <button
-        className="flex-none h-10 px-4 py-2 bg-white rounded-r-full"
+        className='flex-none h-10 px-4 py-2 bg-white rounded-r-full'
         onClick={() => handleSubmit}
       >
         <MagnifyingGlassIcon
           width={20}
           height={20}
-          className="stroke-multistore_blue-light stroke-[2px]"
+          className='stroke-multistore_blue-light stroke-[2px]'
         />
       </button>
     </form>
@@ -84,23 +84,23 @@ function Header({ data }: { data: {} }) {
 
   dispatch(googleUpdate(data));
   return (
-    <header className="z-50 fixed w-full">
+    <header className='z-50 fixed w-full'>
       {/* Top nav */}
-      <div className="grid grid-rows-2 grid-flow-col gap-4 items-center mx-1 h-32 bg-[#F9F9F9] md:flex md:flex-row md:h-20 md:px-5 shadow-lg z-50">
+      <div className='grid grid-rows-2 grid-flow-col gap-4 items-center mx-1 h-32 bg-[#F9F9F9] md:flex md:flex-row md:h-20 md:px-5 shadow-lg z-50'>
         <MultistoreLogo />
         <SearchBar />
         <Link
-          href={"/prodotti"}
+          href={'/prodotti'}
           className={` ${
-            pathname === "/prodotti"
-              ? "hidden"
-              : "absolute left-7 top-[30px] md:relative md:flex md:flex-col md:gap-[2px] md:top-0 md:left-0 cursor-pointer"
+            pathname === '/prodotti'
+              ? 'hidden'
+              : 'absolute left-7 top-[30px] md:relative md:flex md:flex-col md:gap-[2px] md:top-0 md:left-0 cursor-pointer'
           } ${styles.home}`}
         >
           <HomeIcon height={24} />
-          <p className="hidden md:flex font-mono text-xs font-thin">Catalogo</p>
+          <p className='hidden md:flex font-mono text-xs font-thin'>Catalogo</p>
         </Link>
-        <div className="absolute right-7 top-7 md:relative md:flex md:justify-end md:w-1/6 md:top-0 md:right-0 md:items-center md:order-last">
+        <div className='absolute right-7 top-7 md:relative md:flex md:justify-end md:w-1/6 md:top-0 md:right-0 md:items-center md:order-last'>
           <Menu />
         </div>
       </div>
